c38e03bc9f992e062d2d477b84c1f8e35241deef
Mark Ragno <mark@ragno76.com>
2014-12-26 13:31:15 -0700

    second push

--- index.php
+++ index.php
@@ -3,25 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <title>Mark Ragno Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='http://fonts.googleapis.com/css?family=Lustria|Lato:400,700|Lobster' rel='stylesheet' type='text/css'>
 </head>
 <body>
-    <div class="page">
+    <div class="page" id="home">
         
         <header>
-            
+            <div class="name"><h1>Mark P Ragno</h1></div>
+            <nav>
+                <!-- <a href="#home">HOME</a> -->
+                <a href="#projects">PROJECTS</a>
+                <a href="#skills">SKILLS</a>
+                <a href="#contact">CONTACT</a>
+            </nav>
         </header>
 
-
-
-
-
-
-
-
-
-        <footer>
+        <div class="projects" id="projects">
+            <h1>PROJECTS</h1>
+            <div class="project"></div>
+            <div class="project"></div>
+            <div class="project"></div>
+            <div class="project"></div>
             
-        </footer>
+        </div>
+
+        <div class="skills" id="skills">
+            <h1>SKILLS</h1>
+            <div class="content">
+                <p>Having only very recently started in web development, I know my skills have a long way to go - and I am looking forward to that journey!</p>
+                <p>I dove in head first by starting with a 9-5, 12-week, fully immersive bootcamp designed to do one thing:  turn me into a web developer.</p>
+                <p>I will admit - it was one of the hardest things I've ever done.  But 1000% worth it.</p>
+                <p>So much information was covered in such a short time span - my brain is still reeling.  Topics from PHP, HTML5, CSS3, JavaScript, Git, MVC Frameworks, to you name it - we either dove in or at least talked about it.</p>
+                <p>And while I am a novice, my confidence grows daily as I continue the disciplined learning on my own time.</p>
+                <p>I am looking forward to my new career and can't wait to learn more!</p>
+            </div>
+        </div>
+
+        <div class="contact" id="contact">
+            <h1>CONTACT ME</h1>
+            <h2>401~391~7500</h2>
+            <h2>mark@ragno76.com</h2>
+        </div>
 
     </div>
 </body>
