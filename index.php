3b9603670da2d5473ba23a53c744ef936ef6220e
Mark Ragno <mark@ragno76.com>
2014-12-26 12:00:23 -0700

    initial push

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mark Ragno Portfolio</title>
+</head>
+<body>
+    <div class="page">
+        
+        <header>
+            
+        </header>
+
+
+
+
+
+
+
+
+
+        <footer>
+            
+        </footer>
+
+    </div>
+</body>
+</html>
