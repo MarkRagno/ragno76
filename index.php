edbab4afa22e5f360700cbdea53bf1ca109b5cb8
Mark Ragno <mark@ragno76.com>
2014-12-27 18:50:29 -0700

    all working - no sound yet

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link href='http://fonts.googleapis.com/css?family=Lustria|Ubuntu+Mono|Lato:400,700|Arvo|Ubuntu:400,500,700|Josefin+Sans|Lobster|Josefin+Slab|Abril+Fatface|Vollkorn' rel='stylesheet' type='text/css'>
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <link rel="text/javascript" href="main.js">
+    <script type="text/javascript" src="main.js"></script>
 </head>
 <body>
     <div class="page" id="home">
@@ -15,11 +15,6 @@
             <h1>Mark</h1>
             <h1>Ragno</h1>
             <p>[ron-yo]</p>
-<!--             <ul>
-                <li>Italian for "spider"</li>
-                <li>BiCentennial Baby</li>
-                <li>Oldest of 5</li>
-            </ul> -->
         </div>
         <div class="verticalnav">
             <nav>
@@ -35,26 +30,27 @@
             <p>“Life is either a daring adventure - or nothing at all.”  ~ Helen Keller</p>
             <div class="intro section">
                 <div class="introcontent">
-                    <p>I just left a career I was in for over 12 years to become a web developer.
+                    <p>I took the biggest risk of my life and left a 12 year career to become a web developer.
                         <br> 
-                    I'm now on a mission to learn <em>everything</em> about it.</p>
+                    I'm now on a mission...</p>
+                    <p>(a fun one)</p>
                 </div>
             </div>
             <div class="myphoto">
             </div>
         </div>
-        <div class="maincontent">
+        <div class="main">
             <div class="skills section">
                     
                 <div class="skillcontent"  id="skills">
                     <h1 class="mainheader">SKILLS</h1>
-                    <p>My adventure started at a boot camp that taught: 
+                    <p>The adventure started at an awesome boot camp where I was submersed in: 
                         <br>
-                    <span class="emphasis">PHP, HTML5, CSS3, Unix, JavaScript, MySQL, Apache, Git, MVC, Virtual Hosting, Responsive Design, SASS, Jekyll, Handlebars...</span>
+                    <span class="emphasis">PHP, HTML5, CSS3, Unix, JavaScript, MySQL, Apache, Git, MVC, ReptileForms, Bower, NPM, AJAX, JSON, Virtual Hosting, Responsive Design, SASS, Jekyll, Handlebars...</span>
                         <br>
                      you name it - we were discussing it and/or trying it.</p>
                         <br>
-                    <p>During that time, I found that I learned the most from creating projects.</p>
+                    <p>During that time, I found that I learned the most from creating projects...</p>
                 </div>
             </div>
         </div>
@@ -62,24 +58,42 @@
             <div class="projects section" id="projects">
                 <h1 class="mainheader">PROJECTS</h1>
                 <div class="project a">
-                    <!-- <img src="kalquelordSS.png" alt=""> -->
                     <div class="projectdescrip">
-                        <h2>Individual</h2>
+                        <h2 class="projecttitle">Individual</h2>
+                        <div class="projscrip hidden">
+                            <p>Voted the class' best overall project!</p>
+                            <p>A 3rd Grade Math app with sound effects, rich visuals, and dynamic equation creation.</p>
+                            <p>Tools: PHP, HTML, CSS, JavaScript, AJAX, ReptileForms, MVC. Full CRUD with validation.</p>
+                        </div>
                     </div>
                 </div>
                 <div class="project b">
                     <div class="projectdescrip">
-                        <h2>Combined</h2>
-                    </div>
+                        <h2 class="projecttitle">Combined</h2>
+                        <div class="projscrip hidden">
+                            <p>Social Media site aimed towards manly men.</p>
+                            <p>Site gives the ability to post and score manly deeds performed by you and your friends.</p>
+                            <p>Tools: PHP, HTML, CSS, JavaScript, AJAX, ReptileForms, MVC. Full CRUD with validation.</p>
+                        </div>                    </div>
                 </div>
                 <div class="project c">
                     <div class="projectdescrip">
-                        <h2>Non-Profit</h2>
+                        <h2 class="projecttitle">Non-Profit</h2>
+                        <div class="projscrip hidden">
+                            <p>A one day group challenge to update a local non-profit's dated site.</p>
+                            <p>Created a single page design using an attractive color scheme and visuals.</p>
+                            <p>Tools: HTML, CSS, JavaScript.</p>
+                        </div>
                     </div>
                 </div>
                 <div class="project d">
                     <div class="projectdescrip">
-                        <h2>Milestone</h2>
+                        <h2 class="projecttitle">Milestone</h2>
+                        <div class="projscrip hidden">
+                            <p>Mock Twitter Assignment</p>
+                            <p>A 3rd Grade Math app with sound effects, rich visuals, and dynamic equation creation.</p>
+                            <p>Tools: Handlebars, HTML, CSS, JavaScript, jQuery.</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -87,8 +101,8 @@
         <div class="contact section" id="contact">
             <div class="contactinfo">
                 <h1 class="mainheader">Contact</h1>
-                <h2><a href="mailto:mark@ragno76.com">mark@ragno76.com</a></h2>
-                <h2><a href="https://github.com/MarkRagno">github.com/MarkRagno</a></h2>
+                <p><a href="mailto:mark@ragno76.com">mark@ragno76.com</a></p>
+                <p><a href="https://github.com/MarkRagno">github.com/MarkRagno</a></p>
             </div>
         </div>
     </div>
--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-
-    $("div.project").click(function() {
-       $(this).toggleClass('expand');
+    //project sample animation:
+    $('body').on('click','.project', function(){
+        var height = $(this).css('height');
+        if (height == '600px') {
+            $(this).animate({height:'300px', width:'400px', scrollTop: $(this).offset().top
+            },'slow');
+            $(this).find('.projecttitle').removeClass('hidden');
+            $(this).find('.projscrip').addClass('hidden');
+        } else {
+            $(this).animate({height:'600px', width:'800px', scrollTop: $(this).offset().top
+            },'slow');
+            $(this).find('.projscrip').removeClass('hidden');
+            $(this).find('.projecttitle').addClass('hidden');
+        }   
     });
 
+    //mouse fade color:
+    $('a').mouseleave(function() {
+        $(this).fadeTo('slow', 1)
+    });
 
-
-
-
-
-
-
-
-
+    //smooth scrolling to links on page:
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 2000);
+        return false;
+      }
+    }
+  });
 
 });  
